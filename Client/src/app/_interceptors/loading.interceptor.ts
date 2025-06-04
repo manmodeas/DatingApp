b7b82bd2b2a22df3d31779abeadfd861baf0be7b
Aman Manmode <amanmanmode27.am@gmail.com>
2025-06-04 12:38:17 +0530

    Removed fake delay

--- Client/src/app/_interceptors/loading.interceptor.ts
+++ Client/src/app/_interceptors/loading.interceptor.ts
@@ -9,7 +9,6 @@ export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   busyService.busy();
 
   return next(req).pipe(
-    delay(1000),
     finalize(() => {
       busyService.idle()
     })
